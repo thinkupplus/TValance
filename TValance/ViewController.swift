cca96c46ff8ed6ea81005dc0a2da5f99953ea7f2
thinkupplus@gmail.com <thinkupplus@gmail.com>
2020-09-01 18:36:45 +0900

    화면 그리기 테스트 완료

--- TValance/ViewController.swift
+++ TValance/ViewController.swift
@@ -35,16 +35,16 @@ class ViewController: UIViewController {
     }
     
     func initbubble() {
-        bubble = BubbleView(frame: imgView.frame)
-        
-        bubble.backgroundColor = .clear
-        self.view.addSubview(bubble)
-        
+//        bubble = BubbleView(frame: imgView.frame)
+//
+//        bubble.backgroundColor = .clear
+//        self.view.addSubview(bubble)
+//
         hBubble = BubbleView(frame: horView.frame)
         hBubble.backgroundColor = .clear
 //        horView.addSubview(hBubble)
         self.view.addSubview(hBubble)
-        
+//
         vBubble = BubbleView(frame: verView.frame)
         vBubble.backgroundColor = .clear
         self.view.addSubview(vBubble)
@@ -107,43 +107,85 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func btn_Right_T(_ sender: UIButton) {
+        let horRoundBox = horView.frame.size
+        //        let verRoundBox = verView.frame.size
+        //        let horPostion = horView.frame
+        //
+        //        print("X: \(horPostion.origin.x), Y:\(horPostion.origin.y)")
+        //        print("width: \(horPostion.size.width) height: \(horPostion.size.height)")
+                
+        //        let hbubbleX = horPostion.origin.x
+        //        let hbubbleY = horPostion.origin.y
+                
+        let horRoundBboxWidth = ((horRoundBox.width / 2) - 15)
+        
+        let transx1 : CGFloat = horRoundBboxWidth * -1.0
+        let transx2 : CGFloat = horRoundBboxWidth * 1.0
+        print("width: \(horRoundBox.width) height: \(horRoundBox.height)")
+        print("transx1: \(transx1) transx2: \(transx2)")
+        
+        let move = CGAffineTransform(translationX: transx2, y: 0)
+
+        self.hBubble.transform = move
+
+//        UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
+//        //             self.hBubble.frame.origin.x = 20
+//        //                self.hBubble.bounds.origin.y = 5
+//        //                self.hBubble.bounds = CGRect(x: 10, y: 5, width: 20, height: 20)
+//                        let move = CGAffineTransform(translationX: transx2, y: 0)
+//
+//                        self.hBubble.transform = move
+//                    }, completion: nil)
+    }
     @IBAction func btn_TouchUP_circle(_ sender: UIButton) {
         
         print("Touch")
         // 각각의 이미지 사이즈를 계산해서 넘어가지 않도록 해야 한다.
         
 //        let circleSize = imgView.frame.size
-//        let horRoundBox = horView.frame.size
+        let horRoundBox = horView.frame.size
 //        let verRoundBox = verView.frame.size
-        let horPostion = horView.frame
+//        let horPostion = horView.frame
+//
+//        print("X: \(horPostion.origin.x), Y:\(horPostion.origin.y)")
+//        print("width: \(horPostion.size.width) height: \(horPostion.size.height)")
         
-        print("X: \(horPostion.origin.x), Y:\(horPostion.origin.y)")
+//        let hbubbleX = horPostion.origin.x
+//        let hbubbleY = horPostion.origin.y
         
-        let hbubbleX = horPostion.origin.x
-        let hbubbleY = horPostion.origin.y
+        let horRoundBboxWidth = ((horRoundBox.width / 2) - 20)
         
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
-            self.hBubble.frame.origin.x = hbubbleX + 73
-            self.hBubble.frame.origin.y = hbubbleY
-//            self.hBubble.bounds = CGRect(x: 120, y: 5, width: 20, height: 20)
-        }, completion: { _ in
-            UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
-             self.hBubble.frame.origin.x = 20
-//                self.hBubble.bounds.origin.y = 5
-//                self.hBubble.bounds = CGRect(x: 10, y: 5, width: 20, height: 20)
-            }, completion: nil)
-        })
+        let transx1 : CGFloat = horRoundBboxWidth * -1.0
+        let transx2 : CGFloat = horRoundBboxWidth * 1.0
+        print("width: \(horRoundBox.width) height: \(horRoundBox.height)")
+        print("transx1: \(transx1) transx2: \(transx2)")
+        
+        let move = CGAffineTransform(translationX: transx1, y: 0)
+        
+        self.hBubble.transform = move
+        
+//        UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
+//
+//            let move = CGAffineTransform(translationX: transx1, y: 0)
+//
+//            self.hBubble.transform = move
+//
+////            self.hBubble.frame.origin.x = hbubbleX + 73
+////            self.hBubble.frame.origin.y = hbubbleY
+////            self.hBubble.bounds = CGRect(x: 120, y: 5, width: 20, height: 20)
+//        }, completion: nil)
         
         
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
-            self.bubble.frame.origin.x = 120
-            self.bubble.frame.origin.y = 120
-        }, completion: { _ in
-            UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
-                self.bubble.frame.origin.x = 200
-                self.bubble.frame.origin.y = 300
-            }, completion: nil)
-        })
+//        UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
+//            self.bubble.frame.origin.x = 120
+//            self.bubble.frame.origin.y = 120
+//        }, completion: { _ in
+//            UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
+//                self.bubble.frame.origin.x = 200
+//                self.bubble.frame.origin.y = 300
+//            }, completion: nil)
+//        })
     }
 }
 
